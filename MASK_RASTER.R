0ec378273edcf04db42e384f0eb5f4fece8bbe5a
Alicia Pizarro <43381268+APizarro8@users.noreply.github.com>
2019-03-13 15:36:45 +0100

    Update MASK_RASTER.R

--- MASK_RASTER.R
+++ MASK_RASTER.R
@@ -6,22 +6,22 @@ require(rgdal)
 ################ RECORTAR RASTER CON SHAPE #####################
 
 #DIRECTORIO DE TRABAJO
-setwd('D:/TFM_BIOMASA/Prueba_2')
+setwd('XXXX')
 getwd()
 
 #OBTENER LISTA DE ARCHIVOS EN CARPETA
 b<- list.files()
 
 #ENUMERAR ARCHIVO A RECORTAR SEGUN SU NUMERACION EN CARPETA 
-r<- brick(b[16])
+r<- brick(b[X])
 
 #CAPA SHAPE PARA EL RECORTE
-shape<- shapefile('Encinares.shp')
+shape<- shapefile('XXXX.shp')
 plot(shape)
 gredos<- mask(r,shape)
 plot(gredos)
 
 #SALIDA RECORTE RASTER
-writeRaster(gredos,filename = 'MDD2_16Z22_clip45.tif')
+writeRaster(gredos,filename = 'XXXXX.tif')
 
 ################################################################
